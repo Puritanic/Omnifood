c3855cfeeb956fb908bf732e74a15d3ce4388c90
Puritanic <darktasevski@gmail.com>
2018-02-12 09:50:53 +0100

    Add smooth scrolling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,64 @@
 import $ from './vendor/jquery-3.2.1.min';
+import './vendor/noframework.waypoints.min';
+
+console.log(window.Waypoint);
+
+$(() => {
+  const waypoint = new Waypoint({
+    element: document.getElementById('features'),
+    handler() {
+      $('.nav').toggleClass('fixed');
+    },
+    offset: '20%'
+  });
+
+  $('#js-features-scroll').on('click', () => {
+    $('html, body').animate({ scrollTop: $('#features').offset().top }, 1000);
+  });
+  $('#js-plans-scroll').on('click', () => {
+    $('html, body').animate({ scrollTop: $('#plans').offset().top }, 1000);
+  });
+
+  /** *******  jQuery Smooth scrolling  ********* */
+
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        window.location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        window.location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        let target = $(this.hash);
+        target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000,
+            () => {
+              // Callback after animation
+              // Must change focus!
+              const $target = $(target);
+              $target.focus();
+              if ($target.is(':focus')) {
+                // Checking if the target was focused
+                return false;
+              }
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          );
+        }
+      }
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-	entry: {
-		App: "./app/assets/scripts/App.js", // tell webpack which file should it be looking for to bundle it
-		Vendor: "./app/assets/scripts/Vendor.js"
-	},
-	output: {
-		path: path.resolve(__dirname, "./app/temp/scripts"),
-		filename: "[name].js"
-	},
-	module: {
-		loaders: [{
-			loader: 'babel-loader',
-			query: {
-				presets: [
-					["env", {
-						"targets": {
-							"browsers": ["last 2 versions", "safari >= 7"]
-						}
-					}]
-				]
-			},
-			test: /\.js$/,
-			exclude: /node_modules/
-		}]
-	}
-}
+  entry: {
+    App: './app/assets/scripts/App.js', // tell webpack which file should it be looking for to bundle it
+    Vendor: './app/assets/scripts/Vendor.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './app/temp/scripts'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            [
+              'env',
+              {
+                targets: {
+                  browsers: ['last 2 versions', 'safari >= 7']
+                }
+              }
+            ]
+          ]
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
+};
 
 // todo mocha testing after bundling
